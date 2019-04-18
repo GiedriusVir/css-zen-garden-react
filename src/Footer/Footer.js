04cabe7475a44ed141bbaaa9af798c1b16f88ae4
GiedriusVir <virsilasg@gmail.com>
2019-04-18 19:59:45 +0300

    small fixes

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,7 @@ const footer = () => {
     return(
         <div className="footer">
             <div className="footer-item"><a href="https://github.com/GiedriusVir"><i class="fab fa-github"></i></a></div>
-            <div className="footer-item"><a href="www.linkedin.com/in/giedrius-virsilas-03"><i class="fab fa-linkedin"></i></a></div>
+            <div className="footer-item"><a href="https://www.linkedin.com/in/giedrius-virsilas-03"><i class="fab fa-linkedin"></i></a></div>
         </div>
     )
 }
