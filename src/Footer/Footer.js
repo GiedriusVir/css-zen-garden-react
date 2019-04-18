9d7cf5618cac43f66dd50c1eb385d9385b2821d3
GiedriusVir <virsilasg@gmail.com>
2019-04-18 16:23:19 +0300

    footer update

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,12 +4,8 @@ import './Footer.css';
 const footer = () => {
     return(
         <div className="footer">
-            <div className="footer-item"></div>
-            <div className="footer-item"></div>
-            <div className="footer-item"></div>
-            <div className="footer-item"></div>
-            <div className="footer-item"></div>
-            <div className="footer-item"></div>
+            <div className="footer-item"><a href="https://github.com/GiedriusVir"><i class="fab fa-github"></i></a></div>
+            <div className="footer-item"><a href="www.linkedin.com/in/giedrius-virsilas-03"><i class="fab fa-linkedin"></i></a></div>
         </div>
     )
 }
